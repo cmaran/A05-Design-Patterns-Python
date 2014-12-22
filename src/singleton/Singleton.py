5d751f9c1737c1b07062fa68b6d81a41dd52709b
Marlene Dorfinger <mdorfinger@student.tgm.ac.at>
2014-12-22 21:48:37 +0100

    singleton + protokoll

--- src/singleton/Singleton.py
+++ src/singleton/Singleton.py
@@ -7,13 +7,13 @@ Das Singleton-Pattern sorgt dafür, dass nur eine instance der Klasse erstellt w
 """
 class Singleton():
 
-    def __init__(self, klasse):
+    def __init__(self, klasse): # Klasse ist die, die nach dem @Singleton steht
         self.klasse = klasse
         self.instance = None # beim ersten Aufruf ist die Instance none
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self):
         if self.instance == None: # man überprüft beim Aufruf, instance none ist
-            self.instance = self.klasse(*args, **kwargs) # wenn ja, wird die Klasse neu erstellt
+            self.instance = self.klasse() # wenn ja, wird die Klasse neu erstellt
         return self.instance # die instance wird zurückgegeben
 
 
@@ -22,21 +22,21 @@ class SingletonTest:
     def __init__(self):
         print()
 
-f = SingletonTest() # es werden 2 mal SingletonTests erstellt um zu überprüfen ob sie gleich sind.
-g = SingletonTest()
+a = SingletonTest() # es werden 2 mal SingletonTests erstellt um zu überprüfen ob sie gleich sind (gleiche Instanz)
+b = SingletonTest()
 
-print(f is g) # True > Instance ist gleich > Singleton funktioniert
-print(f) # man kann nun sehen, dass die Speicherstelle bei f und g gleich ist
-print(g)
+print(a is b) # True > Instance ist gleich > Singleton funktioniert
+print(a) # man kann nun sehen, dass die Speicherstelle bei a und b gleich ist
+print(b)
 
 
 class SingletonTest: # diese Klasse hat kein @Singleton
     def __init__(self):
         print()
 
-f = SingletonTest()
-g = SingletonTest()
+a = SingletonTest()
+b = SingletonTest()
 
-print(f is g) # false, weil kein Singleton
-print(f) # man kann sehen, dass die Speicherstelle unterschiedlich ist
-print(g)
+print(a is b) # false, weil kein Singleton
+print(a) # man kann sehen, dass die Speicherstelle unterschiedlich ist
+print(b)
